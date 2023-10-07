122235614e6cf30c88cc531d40c8d7cf6ecbd425
Evan Holtrop <evanholtrop@gmail.com>
2023-10-07 13:27:07 -0400

    create windows setup

---
+++ windows_setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Tools
+winget install "Flow Launcher"
+winget install CodeSector.TeraCopy
+winget install voidtools.Everything
+winget install Google.Chrome
+winget install Microsoft.PowerToys
+winget install REALiX.HWiNFO
+
+# Programming
+winget install OBSProject.OBSStudio
+
+# Streaming
+winget install Jetbrains.Toolbox
+winget install Axosoft.GitKraken
+winget install Microsoft.VisualStudioCode
+
+# Elgato
+winget install Elgato.StreamDeck
+winget install Elgato.ControlCenter
+
+# Media/Fun
+winget install Discord.Discord
+winget install Spotify.Spotify
+winget install Valve.Steam
+
+# Drivers
+winget install Nvidia.GeForceExperience
+
+echo "Install StreamerBot, Universal Control"
