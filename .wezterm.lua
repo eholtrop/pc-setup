cacaf098625548c089d7c5092d0e6b4e98af67bc
Evan Holtrop <evanholtrop@gmail.com>
2024-10-15 17:13:28 -0400

    update wezterm script

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,13 @@
-return {
-    color_scheme = "Dracula (Official)"
-    tab_bar_at_bottom = true,
-    use_fancy_tab_bar = false,
-    window_decorations = "RESIZE"
-}
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Dracula (Official)'
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.window_decorations = 'RESIZE'
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
+config.font = wezterm.font 'MesloLGS Nerd Font Mono'
+config.font_size = 14
+
+return config
