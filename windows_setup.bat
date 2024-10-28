07d2a03978a75e72d7f553f56a6cf508bcb16a1c
Evan Holtrop <evanholtrop@gmail.com>
2024-10-28 00:49:15 -0400

    create windows-setup.bat to replace sh file

---
+++ windows_setup.bat
@@ -0,0 +1,40 @@
+# Tools
+winget install "Flow Launcher"
+winget install CodeSector.TeraCopy
+winget install voidtools.Everything
+winget install Microsoft.PowerToys
+winget install REALiX.HWiNFO
+winget install localsend
+winget install Obsidian.Obsidian
+
+winget install OBSProject.OBSStudio
+
+# terminal
+winget install wez.wezterm
+
+winget install eza-community.eza
+
+
+
+winget install ajeetdsouza.zoxide
+
+# Streaming
+winget install Jetbrains.Toolbox
+winget install Axosoft.GitKraken
+winget install Microsoft.VisualStudioCode
+
+# Elgato
+winget install Elgato.StreamDeck
+winget install Elgato.ControlCenter
+
+# Media/Fun
+winget install Discord.Discord
+winget install Spotify.Spotify
+winget install Valve.Steam
+
+winget install LGUG2Z.komorebi
+winget install LGUG2Z.wkhd
+
+echo "Install StreamerBot, Universal Control, Zen Browser"
+
+echo "Move configs for komorebi and wkhd"
