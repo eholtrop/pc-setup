3278dc2559ff9f81e04c5af7b6ecd151330415d2
Evan Holtrop <evanholtrop@gmail.com>
2024-12-23 16:06:06 -0500

    update fedora changes with osx changes

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -1,17 +1,69 @@
+#!/bin/zsh
+
+appendZshrc () {
+    grep -qxF $1 ~/.zshrc || echo $1 >> ~/.zshrc
+    source ~/.zshrc
+}
+
+install () {
+    sudo dnf install -y $1
+}
+
+flatInstall () {
+    flatpak install -y flathub $1
+}
+
+install zsh
+chsh -s $(which zsh)
+
+install ulauncher
+install wmctrl
+
+install zsh-autosuggestions
+appendZshrc "\"echo \"source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh\" >> \${ZDOTDIR:-$HOME}/.zshrc\"
+
+install zsh-syntax-highlighting
+appendZshrc "\"echo \"source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh\" >> \${ZDOTDIR:-$HOME}/.zshrc\"
 
 
-sudo dnf install -y ulauncher
-sudo dnf install -y wmctrl
+# better cd
+install zoxide
+appendZshrc "alias cd=\"z\""
+
+# better ls
+install eza
+appendZshrc "alias ls=\"eza --icons=always\""
 
 sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
 sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
-sudo dnf install -y 1password
+install 1password
 
-flatpak install -y flathub com.visualstudio.code
-flatpak install -y flathub io.github.zen_browser.zen
-flatpak install -y flathub com.axosoft.GitKraken
-flatpak install -y flathub com.discordapp.Discord
-flatpak install -y flathub com.spotify.client
-flatpak install -y flathub md.obsidian.Obsidian
+flatInstall io.github.zen_browser.zen
+flatInstall com.axosoft.GitKraken
+flatInstall com.discordapp.Discord
+flatInstall com.spotify.client
+flatInstall md.obsidian.Obsidian
+flatInstall dev.zed.Zed
+appendZshrc "alias zed=\"flatpak run dev.zed.Zed\""
 
-firefox https://www.jetbrains.com/toolbox-app
+flatInstall org.wezfurlong.wezterm
+rm ~/.wezterm.lua
+cp .wezterm.lua ~/
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+appendZshrc "'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc"
+# install font-meslo-lg-nerd-font
+
+# fix history
+appendZshrc "HISTFILE=$HOME/.zhistory"
+appendZshrc "SAVEHIST=1000"
+appendZshrc "HISTSIZE=999"
+appendZshrc "setopt share_history"
+appendZshrc "setopt hist_expire_dups_first"
+appendZshrc "setopt hist_ignore_dups"
+appendZshrc "setopt hist_verify"
+appendZshrc "bindkey '^[[A' history-search-backward"
+appendZshrc "bindkey '^[[B' history-search-forward"
+
+# this must be last in zshrc
+appendZshrc "eval \"\$(zoxide init zsh)\""
