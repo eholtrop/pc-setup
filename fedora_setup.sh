7d3234b24fbb757d6352025df9d5a316babe4fc9
Evan Holtrop <evanholtrop@gmail.com>
2024-12-23 20:56:33 -0500

    copy zshrc instead of trying to build it :facepalm:

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -1,10 +1,5 @@
 #!/bin/zsh
 
-appendZshrc () {
-    grep -qxF $1 ~/.zshrc || echo $1 >> ~/.zshrc
-    source ~/.zshrc
-}
-
 install () {
     sudo dnf install -y $1
 }
@@ -20,19 +15,14 @@ install ulauncher
 install wmctrl
 
 install zsh-autosuggestions
-appendZshrc "\"echo \"source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh\" >> \${ZDOTDIR:-$HOME}/.zshrc\"
 
 install zsh-syntax-highlighting
-appendZshrc "\"echo \"source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh\" >> \${ZDOTDIR:-$HOME}/.zshrc\"
-
 
 # better cd
 install zoxide
-appendZshrc "alias cd=\"z\""
 
 # better ls
 install eza
-appendZshrc "alias ls=\"eza --icons=always\""
 
 sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
 sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
@@ -44,26 +34,16 @@ flatInstall com.discordapp.Discord
 flatInstall com.spotify.client
 flatInstall md.obsidian.Obsidian
 flatInstall dev.zed.Zed
-appendZshrc "alias zed=\"flatpak run dev.zed.Zed\""
 
 flatInstall org.wezfurlong.wezterm
 rm ~/.wezterm.lua
 cp .wezterm.lua ~/
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-appendZshrc "'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc"
 # install font-meslo-lg-nerd-font
 
 # fix history
-appendZshrc "HISTFILE=$HOME/.zhistory"
-appendZshrc "SAVEHIST=1000"
-appendZshrc "HISTSIZE=999"
-appendZshrc "setopt share_history"
-appendZshrc "setopt hist_expire_dups_first"
-appendZshrc "setopt hist_ignore_dups"
-appendZshrc "setopt hist_verify"
-appendZshrc "bindkey '^[[A' history-search-backward"
-appendZshrc "bindkey '^[[B' history-search-forward"
+rm ~/.zshrc
+cp fedora-config/.zshrc ~/
 
-# this must be last in zshrc
-appendZshrc "eval \"\$(zoxide init zsh)\""
+source ~/.zshrc
