57ccb41d64dea2fe0c533a42975df58cab0933cc
Evan Holtrop <evanholtrop@gmail.com>
2025-06-12 10:19:05 -0400

    add wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,6 @@ config.use_fancy_tab_bar = false
 config.window_decorations = 'RESIZE'
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
-config.font = wezterm.font 'MesloLGS Nerd Font Mono'
 config.font_size = 14
 
 return config
